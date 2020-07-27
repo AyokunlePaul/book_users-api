8fdab3d6e402b715ebb850a60668270ca0468df3
Ayokunle Paul <ayokunle@ulesson.com>
2020-07-27 22:48:16 +0100

    Added json validation when creating user

--- controllers/user_controller.go
+++ controllers/user_controller.go
@@ -1,12 +1,38 @@
 package controllers
 
 import (
+	"fmt"
+	"github.com/AyokunlePaul/book_user-api/domain/users"
 	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"math/rand"
 	"net/http"
+	"strings"
+	"time"
 )
 
 func CreateUser(context *gin.Context) {
-	context.String(http.StatusNotImplemented, "Endpoint not implemented!")
+	user := users.User{}
+	if bindError := context.ShouldBindJSON(&user); bindError != nil {
+		for _, validationError := range bindError.(validator.ValidationErrors) {
+			context.JSON(http.StatusBadRequest, gin.H{
+				"successful": false,
+				"message":    fmt.Sprintf("%s is missing", strings.Title(strings.ToLower(validationError.Field()))),
+			})
+			return
+		}
+	}
+	rand.Seed(time.Now().UTC().UnixNano())
+	userId := rand.Int63()
+	user.Id = userId
+	user.CreatedAt = time.Now()
+	//TODO Save user to the database here!
+
+	context.JSON(http.StatusCreated, gin.H{
+		"successful": true,
+		"message":    "User has been created successfully",
+		"data":       user,
+	})
 }
 
 func GetUser(context *gin.Context) {
--- domain/users/user.go
+++ domain/users/user.go
@@ -1,1 +1,11 @@
 package users
+
+import "time"
+
+type User struct {
+	Id        int64     `json:"id"`
+	FirstName string    `json:"first_name" binding:"required" tag:"first_name"`
+	LastName  string    `json:"last_name" binding:"required" tag:"last_name"`
+	Email     string    `json:"email" binding:"required" tag:"email"`
+	CreatedAt time.Time `json:"created_at"`
+}
